7fb7258a40641f30d4ec8a8a399c404492a55ceb
Hannah Alverson <alverson98@yahoo.com>
2022-08-04 22:25:31 -0600

    added character arrays & comments in HTML for assignment requirements

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Assignment code here
-
-
+console.log(characters.upperCase);
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -10,8 +8,21 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var characters = {
+  upperCase: ["ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")],
+  lowerCase: ["abcdefghijklmnopqrstuvwxyz".split("")],
+  special: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"],
+};
+
+
+function generatePassword() {
+  // prompt user for length
+  //prompt user to confirm char choices
+  // generate password using inputs
+  // return grnerated password
+}
