07850bb8a4e078744b6cb6769398fd812bcd379b
Hannah Alverson <alverson98@yahoo.com>
2022-08-04 23:18:58 -0600

    added some if statements for prompts

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log(characters.upperCase);
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -13,24 +12,69 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var characters = {
+var characterOptions = {
   upperCase: ["ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")],
   lowerCase: ["abcdefghijklmnopqrstuvwxyz".split("")],
   special: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"],
 };
 
-
 function generatePassword() {
-  prompt("How many characters do you want? Choose between 8-128.")
+  var charCount = prompt(
+    "How many characters do you want? Choose between 8-128."
+  );
 
-  confirm("Do you want uppercase letters? Click 'OK' for yes, and 'Cancel' for no.")
+  if (charCount < 8 || charCount > 128 || charCount === "") {
+    alert("Please choose a number between 8 and 128.");
+  } else if (charCount >= 8 && charCount <= 128) {
+    upperChoice();
+  } else if (charCount === null) {
+    window.close();
+  } else {
+    alert("Please choose a number between 8 and 128.");
+  }
 
-  confirm("Do you want lowercase letters? Click 'OK' for yes, and 'Cancel' for no.")
+  function upperChoice() {
+    var upperChoice = confirm(
+      "Do you want uppercase letters? Click 'OK' for yes, and 'Cancel' for no."
+    );
+  }
 
-  confirm("Do you want special characters? Click 'OK' for yes, and 'Cancel' for no.")
+  if (upperChoice) {
+    var yesUpperCase = true;
+  } else {
+    var noUpperCase = false;
+  }
+
+  var lowerChoice = confirm(
+    "Do you want lowercase letters? Click 'OK' for yes, and 'Cancel' for no."
+  );
+
+  if (lowerChoice) {
+    var yesLowerCase = true;
+  } else {
+    var noLowerCase = false;
+  }
+
+  var numberChoice = confirm(
+    "Do you want any numbers? Click 'OK' for yes, and 'Cancel' for no."
+  );
   
-  // prompt user for length
-  //prompt user to confirm char choices
+  if (numberChoice) {
+    var yesNumber = true;
+  } else {
+    var noNumber = false;
+  }
+
+  var specialChoice = confirm(
+    "Do you want special characters? Click 'OK' for yes, and 'Cancel' for no."
+  );
+
+  if (specialChoice) {
+    var yesSpecial = true;
+  } else {
+    var noSpecial = false;
+  }
+
   // generate password using inputs
   // return grnerated password
 }
