64e374f0ae2b419b444faa03de439bb052c53eb3
Hannah Alverson <alverson98@yahoo.com>
2022-08-05 00:15:00 -0600

    added missing number array, comments for readability, and if statements to make array with requested characters

--- script.js
+++ script.js
@@ -15,15 +15,17 @@ generateBtn.addEventListener("click", writePassword);
 var characterOptions = {
   upperCase: ["ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")],
   lowerCase: ["abcdefghijklmnopqrstuvwxyz".split("")],
+  numbers: ["0123456789".split("")],
   special: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"],
 };
 
 function generatePassword() {
+  // Selecting number of characters
   function characterNumber() {
     var charCount = prompt(
       "How many characters do you want? Choose between 8-128."
     );
-
+    // Forcing user to choose 8 through 128
     if (charCount < 8 || charCount > 128 || charCount === "") {
       alert("Please choose a number between 8 and 128.");
       characterNumber();
@@ -38,37 +40,53 @@ function generatePassword() {
     }
   }
   characterNumber();
-
-  function upperChoice() {
-    var upperChoice = confirm(
-      "Do you want uppercase letters? Click 'OK' for yes, and 'Cancel' for no."
-    );
-    }
+ 
+  // User prompts if they wany uppercase, lowercase, number, and/or special characters
+  var upperChoice = confirm(
+    "Do you want uppercase letters? Click 'OK' for yes, and 'Cancel' for no."
+  );
 
   var lowerChoice = confirm(
     "Do you want lowercase letters? Click 'OK' for yes, and 'Cancel' for no."
   );
 
-
   var numberChoice = confirm(
     "Do you want any numbers? Click 'OK' for yes, and 'Cancel' for no."
   );
-
 
   var specialChoice = confirm(
     "Do you want special characters? Click 'OK' for yes, and 'Cancel' for no."
   );
 
-if (upperChoice === true || lowerChoice === true || numberChoice === true || specialChoice === true) {
-  createPassword();
-} else {
-  alert("You must select at least one character type option. Please try again.");
-characterNumber();
-}
+  // Forcing user to choose at least one character type
+  if (
+    upperChoice === true ||
+    lowerChoice === true ||
+    numberChoice === true ||
+    specialChoice === true
+  ) {
+    createPassword();
+  } else {
+    alert("You must select at least one character type. Please try again.");
+    characterNumber();
+  }
 
-function createPassword() {
  
-if ()
-}
-  
+  function createPassword() {
+     // Empty array to create new array with requested character types
+     var requestChar = [];
+    if (upperChoice) {
+      requestChar = requestChar.concat(characterOptions.upperCase);
+    }
+    if (lowerChoice) {
+      requestChar = requestChar.concat(characterOptions.lowerCase);
+    }
+    if (numberChoice) {
+      requestChar = requestChar.concat(characterOptions.numbers);
+    }
+    if (specialChoice) {
+      requestChar = requestChar.concat(characterOptions.special);
+    }
+    console.log(requestChar);
+  }
 }
